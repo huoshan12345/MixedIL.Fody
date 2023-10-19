cfebb144dd060162deb11ed7e41fe0fc3f7029be
jeremyli <jeremyli@seismic.com>
2023-10-19 16:31:22 +0800

    fix tests

--- test/MixedIL.Tests.AssemblyToProcess/ObjectExtensions.cs
+++ test/MixedIL.Tests.AssemblyToProcess/ObjectExtensions.cs
@@ -24,6 +24,14 @@ public static class ObjectExtensions
     public static extern void SetKeyName(this AssemblyKeyNameAttribute attribute, string keyName);
 
     /// <summary>
+    /// Set the property <see cref="AssemblyKeyNameAttribute.KeyName"/> that doesn't have setter by its BackingField.
+    /// </summary>
+    /// <param name="attribute"></param>
+    /// <param name="keyName"></param>
+    [MixedIL]
+    public static extern void SetKeyNameNet4(this AssemblyKeyNameAttribute attribute, string keyName);
+
+    /// <summary>
     /// Set the property <see cref="TestObject.Value"/> that doesn't have setter by its BackingField.
     /// </summary>
     /// <param name="testObject"></param>
